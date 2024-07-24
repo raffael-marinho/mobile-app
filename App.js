ad74e4458c7d1258c2d3485a83b47399749fdfb1
raffael-marinho <silvamarinho10@gmail.com>
2024-07-23 21:03:54 -0300

    adiciona a função App

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+export default function App(params) {
+  return(
+    <View style={style.container}>
+      <Text>Sujeito Programador</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: "red"
+  }
+})
